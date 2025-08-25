855b2fa5dbe8eafab8db4f9abcebc252a901a695
Polisetti Shruti <polishettyshruti@gmail.com.com>
2025-08-25 20:54:31 +0530

    Fixed responsive navbar in App.js

--- src/App.js
+++ src/App.js
@@ -9,13 +9,27 @@ function App() {
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
         <div className="container">
           <Link className="navbar-brand" to="/">URL Shortener</Link>
-          <div className="collapse navbar-collapse">
+
+         
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+
+          <div className="collapse navbar-collapse" id="navbarNav">
             <ul className="navbar-nav ms-auto">
               <li className="nav-item">
                 <Link className="nav-link fw-bold text-white" to="/">Home</Link>
               </li>
               <li className="nav-item">
-                <Link className="nav-link text-white fw-bold" to="/admin">Admin</Link>
+                <Link className="nav-link fw-bold text-white" to="/admin">Admin</Link>
               </li>
             </ul>
           </div>
@@ -31,4 +45,5 @@ function App() {
 }
 
 export default App;
+
 
